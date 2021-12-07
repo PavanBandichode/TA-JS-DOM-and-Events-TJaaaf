3af8ed02737d9d1e71ab4ebbf01112c93b101667
PavanBandichode <pavan.bandichode@gmail.com>
2021-12-07 13:26:04 +0530

    movie checklist assignment

--- block-BJaaet/code/script.js
+++ block-BJaaet/code/script.js
@@ -1,1 +1,29 @@
-let uerInput = "";
+let inputVal = document.querySelector(`input[type="text"]`);
+let initialValue = "";
+inputVal.value = initialValue;
+let ul = document.querySelector(".output");
+function movieInputHandler() {
+  let li = document.createElement("li");
+  let input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = "1";
+  let label = document.createElement("label");
+  label.for = "1";
+  label.innerText = inputVal.value;
+  var span = document.createElement("span");
+  span.classList.add("removeIt");
+  span.innerText = "x";
+  li.append(input, label, span);
+  inputVal.value = initialValue;
+  return li;
+}
+inputVal.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    ul.append(movieInputHandler());
+  }
+});
+ul.addEventListener("click", function (event) {
+  if (event.target.classList.contains("removeIt")) {
+    event.target.parentElement.remove();
+  }
+});
